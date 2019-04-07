921fdadff77157c89bf620cc454b366b97e52ca6
Arshia Malek <46817588+Ashmlk@users.noreply.github.com>
2019-04-07 00:32:23 -0400

    Initial test methods added

--- SirenRecords/src/Playlist.java
+++ SirenRecords/src/Playlist.java
@@ -1,16 +1,25 @@
 import java.util.*;
-
+import static org.junit.Assert.*;				
+import org.junit.Test;
 public class Playlist implements java.io.Serializable {
 
     LinkedList<Song> playlist;
-
+    
     public Playlist(){
         playlist = new LinkedList<Song>();
+ 
     }
 
     public boolean add(Song song){
         return this.playlist.add(song);
     }
+    
+    /*
+     * Gets the most recent(last) song added to the playlist
+     */
+  public Song getRecentSong() {
+	  return this.playlist.getLast();
+  }
     
     /*
      * Sort playlist based on Song's name
@@ -53,14 +62,115 @@ public class Playlist implements java.io.Serializable {
     public boolean delete(Song song) { 
     	return this.playlist.remove(song);
     	}
+    public boolean searchPlaylist(Playlist playlist) {
+    	if(playlist != null)
+    		return true;
+    	else
+    		return false;
+    }
 
     @Override
     public String toString(){
-        StringBuilder string = new StringBuilder();
+        StringBuilder string = new StringBuilder("Playlist:\n");
         for(int i=0; i<this.playlist.size(); i++){
             string.append(this.playlist.get(i).toString());
             string.append("\n");
         }
         return string.toString();
     }
+    /*
+     * Testers for testing each methods of our playlist object
+     */
+    @Test
+    public void testLinkedList() {
+    	Playlist playlist = new Playlist();
+    	assertEquals(null,playlist.playlist.get(0));
+    }
+    public void testAdd() {
+    	Playlist list = new Playlist();
+    	Song song = new Song("Test", "TestArtist", 2019, 560);
+    	list.add(song);
+    	assertEquals(song, list.getRecentSong());
+    }
+    public void testDelete() {
+    	Playlist list = new Playlist();
+    	Song song = new Song("Test", "TestArtist", 2019, 560);
+    	Song song2 = new Song("Test2", "TestArtist2", 2020, 570);
+    	list.add(song);
+    	list.add(song2);
+    	list.delete(song2);
+    	assertEquals(song, list.getRecentSong());
+    }
+    public void testSortByName() {
+    	Playlist list = new Playlist();
+    	Playlist sortedList = new Playlist();
+    	Song song = new Song("Test", "TestArtist", 2019, 560);
+    	Song song2 = new Song("ATest2", "TestArtist2", 2020, 570);
+    	list.add(song);
+    	list.add(song2);
+    	sortedList.add(song2);
+    	sortedList.add(song);
+    	list.sortByName();
+    	assertEquals(sortedList,list);
+    }
+    public void testSortByArtist() {
+    	Playlist list = new Playlist();
+    	Playlist sortedList = new Playlist();
+    	Song song = new Song("Test", "TestArtist", 2019, 560);
+    	Song song2 = new Song("ATest2", "ATestArtist2", 2020, 570);
+    	list.add(song);
+    	list.add(song2);
+    	sortedList.add(song2);
+    	sortedList.add(song);
+    	list.sortByArtist();
+    	assertEquals(sortedList,list);
+    }
+    public void testSortByYearOldtoNew() {
+    	Playlist list = new Playlist();
+    	Playlist sortedList = new Playlist();
+    	Song song = new Song("Test", "TestArtist", 2019, 560);
+    	Song song2 = new Song("ATest2", "ATestArtist2", 2018, 570);
+    	list.add(song);
+    	list.add(song2);
+    	sortedList.add(song2);
+    	sortedList.add(song);
+    	list.sortByYearOldtoNew();
+    	assertEquals(sortedList,list);
+    }
+    public void testSortByYearNewtoOld() {
+    	Playlist list = new Playlist();
+    	Playlist sortedList = new Playlist();
+    	Song song = new Song("Test", "TestArtist", 2019, 560);
+    	Song song2 = new Song("ATest2", "ATestArtist2", 2018, 570);
+    	list.add(song);
+    	list.add(song2);
+    	sortedList.add(song);
+    	sortedList.add(song2);
+    	list.sortByYearNewtoOld();
+    	assertEquals(sortedList,list);
+    }
+    public void testSortByLength() {
+    	Playlist list = new Playlist();
+    	Playlist sortedList = new Playlist();
+    	Song song = new Song("Test", "TestArtist", 2019, 560);
+    	Song song2 = new Song("ATest2", "ATestArtist2", 2018, 570);
+    	list.add(song2);
+    	list.add(song);
+    	sortedList.add(song);
+    	sortedList.add(song2);
+    	list.sortByLength();
+    	assertEquals(sortedList,list);
+    }
+    public void testToString() {
+    	Playlist list = new Playlist();
+    	Song song = new Song("Test", "TestArtist", 2019, 560);
+    	Song song2 = new Song("ATest2", "ATestArtist2", 2018, 570);
+    	list.add(song);
+    	list.add(song2);
+    	StringBuilder playlistAsString = new StringBuilder(song.toString()); 
+    	playlistAsString.append("\n");
+    	playlistAsString.append(song2.toString());
+    	assertEquals(playlistAsString,list.toString());
+    }
+
 }
