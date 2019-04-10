fa6e36c3d13bfc1e5d4df9d143e0ad3f5d363247
dawson-brown <dawsonstbrown@gmail.com>
2019-04-10 17:52:52 -0400

    refactor account tests

--- SirenRecords/tests/TestAccount.java
+++ SirenRecords/tests/TestAccount.java
@@ -12,34 +12,39 @@ public class TestAccount {
 	private Playlist test1;
 	private Playlist test2;
 	private Playlist test3;
-	private Song inTest1;
-	private Song inTest2;
-	private Song inTest3;
+	private Song song1;
+	private Song song2;
+	private Song song3;
 @Before
 public void setUp() throws IOException {
 	tester = new Account("Test");
 	test1 = new Playlist();
 	test2 = new Playlist();
 	test3 = new Playlist();
-	inTest3 = new Song("ATest", "ATest", 0, 0);
+	song1 = new Song("First", "Song", 0, 0);
+	song2 = new Song("Second", "Song", 0, 0);
+	song3 = new Song("Third", "Song", 0, 0);
 }
 @Test
 public void testAddPlaylist() {
 	tester.addPlaylist("Testtest1");
-	tester.addPlaylist("Testtest2");
-	tester.addPlaylist("Testtest3");
 	assertEquals(true, tester.selectPlayList("Testtest1"));
 	assertEquals(false, tester.selectPlayList("TestInvalid"));
 }
 @Test
 public void testAddToSong() {
-	tester.addToPlayList(inTest3);
-	assertEquals(inTest3.toString(), test3.getRecentSong().toString());
+	tester.addPlaylist("Testtest2");
+	tester.selectPlayList("Testtest2");
+	tester.addToPlayList(song3);
+	assertEquals(song3.toString(), tester.getCurrent_list().getRecentSong().toString());
 }
 @Test
 public void testRemoveSongFromPlaylist() {
-	tester.removeFromPlayList(inTest3);
-	assertEquals(null,test3.getRecentSong());
+	tester.addPlaylist("Testtest3");
+	tester.addToPlayList(song2);
+	tester.addToPlayList(song3);
+	tester.removeFromPlayList(song3);
+	assertEquals(song2,tester.getCurrent_list().getRecentSong());
 }
 @Test
 public void testGetAccountCurrentPlaylistName() {
